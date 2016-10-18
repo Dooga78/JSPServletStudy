1cb2bb4117852a5a701bb1e3fceec56966c1b8b6
dooga78 <dooga78@gmail.com>
2016-10-18 21:53:30 +0900

    filter, service, vo commit

---
+++ JSPServletStudy/src/com/vo/EmpVO.java
@@ -0,0 +1,48 @@
+package com.vo;
+
+import java.sql.Date;
+
+public class EmpVO {
+
+	String empId;
+	String ename;
+	int salary;
+	Date dateOfHire;
+	
+	public EmpVO() {
+		
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Date getDateOfHire() {
+		return dateOfHire;
+	}
+
+	public void setDateOfHire(Date dateOfHire) {
+		this.dateOfHire = dateOfHire;
+	}
+	
+}
